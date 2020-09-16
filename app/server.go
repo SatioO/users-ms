9f2e079286baeb4a6fa07eb81730561b4b77217e
Vaibhav Satam <vaibhav.satam@accionlabs.com>
2020-09-17 00:34:18 +0530

    removed config

--- app/server.go
+++ app/server.go
@@ -1,12 +1,18 @@
 package app
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 
 	_userDelivery "github.com/satioO/users/app/users/delivery/http"
 	_userRepository "github.com/satioO/users/app/users/repository/mongo"
@@ -19,20 +25,24 @@ import (
 	_authDelivery "github.com/satioO/users/app/auth/delivery/http"
 	_authRepository "github.com/satioO/users/app/auth/repository"
 	_authUsecase "github.com/satioO/users/app/auth/usecase"
-
-	"github.com/satioO/users/app/config"
 )
 
 // Server defines the routing configuration
 type Server struct {
-	Router *mux.Router
-	DB     *mongo.Database
+	HTTPServer *http.Server
+	Router     *mux.Router
+	DB         *mongo.Database
 }
 
 // Initialize the storage and router
 func (a *Server) Initialize() {
-	a.DB = config.ConnectDB("users_ms")
-	a.Router = mux.NewRouter()
+	a.DB = ConnectDB("users_ms")
+	a.Router = mux.NewRouter().StrictSlash(true)
+
+	// Auth
+	authRepo := _authRepository.NewAuthRepository(a.DB)
+	authUsecase := _authUsecase.NewAuthUsecase(authRepo)
+	_authDelivery.MakeAuthHandlers(a.Router, authUsecase)
 
 	// Users
 	usersRepo := _userRepository.NewUsersRepository(a.DB)
@@ -44,14 +54,56 @@ func (a *Server) Initialize() {
 	booksUsecase := _bookUsecase.NewBookUsecase(booksRepo)
 	_bookDelivery.MakeBookHandlers(a.Router, booksUsecase)
 
-	// Auth
-	authRepo := _authRepository.NewAuthRepository(a.DB)
-	authUsecase := _authUsecase.NewAuthUsecase(authRepo)
-	_authDelivery.MakeAuthHandlers(a.Router, authUsecase)
 }
 
 // Run the application
-func (a *Server) Run(port string) {
+func (a *Server) Run(port string) error {
 	fmt.Println("User Management Service is Up Now")
-	log.Fatal(http.ListenAndServe(port, a.Router))
+
+	// HTTP Server
+	a.HTTPServer = &http.Server{
+		Addr:           port,
+		Handler:        a.Router,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	go func() {
+		if err := a.HTTPServer.ListenAndServe(); err != nil {
+			log.Fatalf("Failed to listen and serve: %+v", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, os.Interrupt)
+
+	<-quit
+
+	ctx, shutdown := context.WithTimeout(context.Background(), 5*time.Second)
+	defer shutdown()
+
+	return a.HTTPServer.Shutdown(ctx)
+}
+
+// ConnectDB estabilishes the connection with the database
+func ConnectDB(dbName string) *mongo.Database {
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, err := mongo.NewClient(clientOptions)
+
+	//Set up a context required by mongo.Connect
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+
+	//To close the connection at the end
+	defer cancel()
+
+	err = client.Ping(context.Background(), readpref.Primary())
+	if err != nil {
+		log.Fatal("Couldn't connect to the database", err)
+	} else {
+		log.Println("Connected!")
+	}
+
+	return client.Database(dbName)
 }
